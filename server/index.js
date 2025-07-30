c109a1b92b2a25f995f2e6401c85b9dfca3ea84b
chips2314 <skidanova_2003@mail.ru>
2025-07-30 15:34:28 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -94,6 +94,8 @@ app.delete('/tasks/:id', (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Сервер запущен: http://localhost:${PORT}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Сервер запущен на порту ${PORT}`);
 });
